3c8ebecdced11fdd154fe0107c768d6cef035976
vmemail <vmemail@inbox.com>
2016-07-10 11:12:19 -0300

    Add: testclass

---
+++ app/src/main/java/gitapp1/myappgit1/TestClass.java
@@ -0,0 +1,14 @@
+package gitapp1.myappgit1;
+
+/**
+ * Created by u on 10/07/16.
+ */
+public class TestClass {
+    // this is some class
+    TestClass(){
+        somemethod();
+    }
+    private void somemethod(){
+        System.out.println("somestuff");
+    }
+}
